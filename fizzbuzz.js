85c135402e80729884e8ac18124a48fc512c9e63
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-04-27 23:35:52 -0600

    Finishing touches

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,8 @@
+// Function to fizzbuzz inner change
 function fizzbuzz(number) {
+    if (number === 0){
+        return 0;
+    }
     if (number %3 === 0 && number %5 === 0){
         return 'fizzbuzz'
     }
@@ -8,6 +12,18 @@ function fizzbuzz(number) {
     if (number === 5 || number %5 === 0) {
         return 'buzz';
     }
+    return number;
 }
 
+// Function to print the number in console
+function print(number) {
+    for (let i=0; i <= number; i++) {
+        // Please write < node fizzbuzz > in terminal to print
+        console.log(`${i}. ${fizzbuzz(i)}`)
+    }
+}
+
+print(18);
+
+// To export the document to the test jest
 module.exports = fizzbuzz;
